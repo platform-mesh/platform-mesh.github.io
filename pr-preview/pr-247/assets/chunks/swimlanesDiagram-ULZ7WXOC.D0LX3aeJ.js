416bd70f4f7736c9ee35a5bf195494de117fdd41
gkrajniak <gkrajniak@users.noreply.github.com>
2026-08-13 10:15:15 +0000

    Deploy preview for PR 247 🛫

---
+++ pr-preview/pr-247/assets/chunks/swimlanesDiagram-ULZ7WXOC.D0LX3aeJ.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.DnEPc3Qd.js";import{_ as a}from"../app.CdC3wTF6.js";import"./chunk-5VM5RSS4.iovfhoMF.js";import"./chunk-XXDRQBXY.BA7X1CM1.js";import"./chunk-KBJHAD2P.BQJCmo0X.js";import"./chunk-2GRJ4B5K.B1mulUsm.js";import"./channel.CvLyCNCu.js";import"./framework.Daw7UJeL.js";import"./theme.B7baJcKY.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
