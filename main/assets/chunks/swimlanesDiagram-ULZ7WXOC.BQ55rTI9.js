9fde49d8e4eddb11c9481614dbb00e653834136b
gkrajniak <gkrajniak@gmail.com>
2026-08-13 10:16:21 +0000

    Deploying to gh-pages from @ platform-mesh/platform-mesh.github.io@d4283e722cf7db945872ab2bab943b9f879a7e7a 🚀

---
+++ main/assets/chunks/swimlanesDiagram-ULZ7WXOC.BQ55rTI9.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.B4dz0gsq.js";import{_ as a}from"../app.TRDklHda.js";import"./chunk-5VM5RSS4.DXUggqBd.js";import"./chunk-XXDRQBXY.CJENuvDv.js";import"./chunk-KBJHAD2P.DdiWB3q0.js";import"./chunk-2GRJ4B5K.BJqzcKKS.js";import"./channel.Cz6wm7Vj.js";import"./framework.CJsA7bxt.js";import"./theme.CjdMSdgr.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
