6d75d72817635ca8755dcaacfbf9b4eaeb2841eb
ntnn <10514301+ntnn@users.noreply.github.com>
2026-08-28 07:51:01 +0000

    Deploying to gh-pages from @ platform-mesh/platform-mesh.github.io@5bc9acd60c7b53145208ddc2e972f5a8a108f6c1 🚀

---
+++ main/assets/chunks/swimlanesDiagram-ULZ7WXOC.DM79SdMK.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.m1KbcjH0.js";import{_ as a}from"../app.DnB8gUnf.js";import"./chunk-5VM5RSS4.DJpfgrge.js";import"./chunk-XXDRQBXY.FW0vG39v.js";import"./chunk-KBJHAD2P.D7dLwDVH.js";import"./chunk-2GRJ4B5K.GBU0CEON.js";import"./channel.BfWfEQE3.js";import"./framework.CJsA7bxt.js";import"./theme.1H_-tRA0.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
