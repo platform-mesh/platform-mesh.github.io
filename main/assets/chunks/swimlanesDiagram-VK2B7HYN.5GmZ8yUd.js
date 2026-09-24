c236cbba5ec7a0b7fe7ce77f362cd7e3e1a72c64
aaronschweig <42006873+aaronschweig@users.noreply.github.com>
2026-09-24 15:43:07 +0000

    Deploying to gh-pages from @ platform-mesh/platform-mesh.github.io@8ab865167d415ab4ce9c2096d564b7a2fc95f497 🚀

---
+++ main/assets/chunks/swimlanesDiagram-VK2B7HYN.5GmZ8yUd.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-A5DVABFB.CySobP5d.js";import{_ as a}from"../app.DITkApVV.js";import"./chunk-5VM5RSS4.kcz-LYeK.js";import"./chunk-XXDRQBXY.BiJiRZSN.js";import"./chunk-POPQ4Y6H.B4LNRX9I.js";import"./chunk-F27PBJKO.DxOUpckN.js";import"./channel.DlPYOtKx.js";import"./framework.d5iMQeUW.js";import"./theme.DDARK5wM.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
